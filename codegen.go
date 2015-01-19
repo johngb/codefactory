fbb65045f6d7d6b5bb77e4215555a541a771e433
John Beckett <jgbeckett@gmail.com>
2015-01-19 20:22:48 +0100

    initial tests

--- codegen.go
+++ codegen.go
@@ -2,7 +2,7 @@ package codegen
 
 import (
 	"errors"
-	"fmt"
+	"math/rand"
 	"strings"
 	"unicode"
 )
@@ -15,12 +15,14 @@ const (
 )
 
 var (
-	errDuplicates   = errors.New("can't include duplicates")
-	errWhitespace   = errors.New("can't include whitespace")
-	errAlreadyExist = errors.New("can't extend with characters that already exist")
-	errNotUpper     = errors.New("not uppercase letters")
-	errNotLower     = errors.New("not lower letters")
-	errInvalidFormat = errors.New("invalid format character")
+	errDuplicates        = errors.New("can't include duplicates")
+	errWhitespace        = errors.New("can't include whitespace")
+	errAlreadyExist      = errors.New("can't extend with characters that already exist")
+	errNotUpper          = errors.New("not uppercase letters")
+	errNotLower          = errors.New("not lower letters")
+	errInvalidFormat     = errors.New("invalid format character")
+	errNotLetterOrNumber = errors.New("not a letter or a number")
+	errNotLatin1         = errors.New("can only extend with Latin1 letters and digits")
 )
 
 type CodeFactory struct {
@@ -45,17 +47,23 @@ func New() *CodeFactory {
 	}
 }
 
+func NewReadable() *CodeFactory {
+	c := New()
+	c.Exclude("IlO")
+	return c
+}
+
 func (c *CodeFactory) Exclude(s string) error {
 	for _, v := range s {
-		switch v {
-		case v >= '0' && v <= '9': // number
+		switch {
+		case unicode.IsDigit(v):
 			c.num = strings.Trim(c.num, v)
-		case v >= 'a' && v <= 'z': // lower
+		case unicode.IsLower(v):
 			c.lower = strings.Trim(c.lower, v)
-		case v >= 'A' && v <= 'Z': // upper
+		case unicode.IsUpper(v): // upper
 			c.upper = strings.Trim(c.upper, v)
 		default:
-			return fmt.Errorf("the character %q is not a letter or a number", v)
+			return errNotLetterOrNumber
 		}
 	}
 	return nil
@@ -72,87 +80,53 @@ func (c *CodeFactory) SetCustom(s string) error {
 }
 
 func (c *CodeFactory) SetFormat(s string) error {
-    for _, v := range s {
-    	// if not punctuation, symbol, or space
-    	if !unicode.IsPunct(v) && !unicode.IsSymbol(v) && != ' ' {
-    		return errInvalidFormat
-    	}
-    }
+	for _, v := range s {
+		// if not punctuation, symbol, or space
+		if !unicode.IsPunct(v) && !unicode.IsSymbol(v) && v != ' ' {
+			return errInvalidFormat
+		}
+	}
 	c.format = s
 	return nil
 }
 
-func (c *CodeFactory) ExtendUpper(s string) error {
-	if hasWhitespace(s) {
+func (c *CodeFactory) Extend(s string) error {
+	if !allLatin1(s) {
+		return errNotLatin1
+	} else if hasWhitespace(s) {
 		return errWhitespace
 	} else if hasDuplicates(s) {
 		return errDuplicates
-	} else if hasDuplicates(s + c.upper) {
-		return errAlreadyExist
-	} else if !allUpper(s) {
-		return errNotUpper
 	}
-	c.upper += s
+
+	for _, v := range s {
+
+		switch {
+		// lowercase
+		case unicode.IsLower(v):
+			if strings.Contains(c.lower, string(v)) {
+				return errDuplicates
+			}
+			c.lower += string(v)
+
+		case unicode.IsUpper(v):
+			if strings.Contains(c.upper, string(v)) {
+				return errDuplicates
+			}
+			c.upper += string(v)
+
+		case unicode.IsDigit(v):
+			if strings.Contains(c.num, string(v)) {
+				return errDuplicates
+			}
+			c.num += string(v)
+
+		default:
+			return errNotLetterOrNumber
+
+		}
+	}
 	return nil
-}
-
-func (c *CodeFactory) ExtendLower(s string) error {
-	if hasWhitespace(s) {
-		return errCustomHasWhitespace
-	} else if hasDuplicates(s) {
-		return errCustomHasDuplicates
-	} else if hasDuplicates(s + c.lower) {
-		return errDuplicateOfUpper
-	} else if !allLower(s) {
-		return errNotLower
-	}
-	c.upper += s
-	return nil
-}
-
-func hasWhiteSpace(s string) bool {
-	for _, c := range s {
-		if unicode.IsSpace(rune(c)) {
-			return true
-		}
-	}
-	return false
-}
-
-func allUpper(s string) bool {
-	for _, c := range s {
-		if !unicode.IsUpper(c) {
-			return false
-		}
-	}
-	return true
-}
-
-func allLower(s string) bool {
-	for _, c := range s {
-		if !unicode.IsLower(c) {
-			return false
-		}
-	}
-	return true
-}
-
-// func hasNonASCII(s string) bool {
-// 	for _, c := range s {
-// 		if rune(c) > unicode.MaxASCII {
-// 			return true
-// 		}
-// 	}
-// 	return false
-// }
-
-func hasDuplicates(s string) bool {
-	for _, c := range s {
-		if strings.Count(s, c) != 1 {
-			return true
-		}
-	}
-	return false
 }
 
 func (c *CodeFactory) lenNum() int {
@@ -172,38 +146,106 @@ func (c *CodeFactory) lenLetter() int {
 	return total
 }
 
-// func unique(s string) bool {
-// 	for _, c := range s {
-// 		count := strings.Count(s, c); if count != 1 {
-// 			return false
-// 		}
-// 	}
-// 	return true
-// }
+func (c *CodeFactory) MakeSingle() string {
 
-// func isLower(s string) bool {
-// 		for _, c := range s {
-// 		if s < 'a' || s > 'z' {
-// 			return false
-// 		}
-// 	}
-// 	return true
-// }
+	// strings to build codes from
+	x := c.num + c.upper + c.lower
+	d := c.num
+	l := c.lower
+	u := c.upper
+	a := c.upper + c.lower
 
-// func isUpper(s string) bool {
-// 		for _, c := range s {
-// 		if s < 'A' || s > 'Z' {
-// 			return false
-// 		}
-// 	}
-// 	return true
-// }
+	// lengths for optimisation
+	lenX := len(x)
+	lenD := len(d)
+	lenL := len(l)
+	lenU := len(u)
+	lenA := len(a)
 
-// func isNumber(s string) bool {
-// 		for _, c := range s {
-// 		if s < '0' || s > '9' {
-// 			return false
-// 		}
-// 	}
-// 	return true
-// }
+	// result string always starts with a prefix
+	r := c.prefix
+
+	for _, v := range c.format {
+		// formatting symbol
+		if !unicode.IsLetter(v) {
+			r += string(v)
+			continue
+		}
+		// is code character
+		switch v {
+
+		// any character in sets
+		case 'x':
+			r += string(x[rand.Intn(lenX)])
+
+		// any number digit
+		case 'd':
+			r += string(d[rand.Intn(lenD)])
+
+		// any lowercase letter
+		case 'l':
+			r += string(l[rand.Intn(lenL)])
+
+		// any uppercase letter
+		case 'u':
+			r += string(u[rand.Intn(lenU)])
+
+		// any letter (upper or lowercase)
+		case 'a':
+			r += string(a[rand.Intn(lenA)])
+
+		default:
+			panic("John broke the code!")
+		}
+
+	}
+
+	r += c.suffix
+
+	return r
+}
+
+func hasWhiteSpace(s string) bool {
+	for _, v := range s {
+		if unicode.IsSpace(rune(v)) {
+			return true
+		}
+	}
+	return false
+}
+
+func allUpper(s string) bool {
+	for _, v := range s {
+		if !unicode.IsUpper(v) {
+			return false
+		}
+	}
+	return true
+}
+
+func allLower(s string) bool {
+	for _, v := range s {
+		if !unicode.IsLower(v) {
+			return false
+		}
+	}
+	return true
+}
+
+func hasDuplicates(s string) bool {
+	for _, v := range s {
+		if strings.Count(s, v) != 1 {
+			return true
+		}
+	}
+	return false
+}
+
+func allLatin1(s string) bool {
+	for _, v := range s {
+		if v > unicode.MaxLatin1 {
+			return false
+		}
+	}
+	return true
+}
--- codegen_test.go
+++ codegen_test.go
@@ -2,44 +2,57 @@ package codegen
 
 import (
 	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
 )
 
-var testCases = []struct {
-	input     string
-	expected  int
-	expectErr bool
+func TestExclude(t *testing.T) {
+	var testCases = []struct {
+	description string
+	input       string
+	wantNum     string
+	wantLower   string
+	wantUpper   string
+	wantErr     bool
 }{
-	{"1", 1, false},
+	{"1aZ", "023456789", "bcdefghijklmnopqrstuvwxyz", "ABCDEFGHIJKLMNOPQRSTUVWXYZ", false},
+}
+	Convey("Test Exclude", t, func() {
+		for _, test := range testCases {
+			Convey(test.description, func() {
+
+			cf := New()
+			cf.Exclude(test.input)
+			So(cf.num, ShouldResemble, test.wantNum)
+			So(cf.lower, ShouldResemble, test.wantLower)
+			So(cf.upper, ShouldResemble, test.wantUpper)
+
+			if actual != test.expected {
+				t.Fatalf("")
+			}
+
+			// if we expect an error and there isn't one
+			if test.expectErr && err == nil {
+				t.Errorf("Name(%v): expected an error, but error is nil", test.input)
+			}
+			// if we don't expect an error and there is one
+			if !test.expectErr && err != nil {
+				t.Errorf("Name(%v): expected no error, but error is %s", test.input, err)
+			}
+		}
+		})
+	})
 }
 
-func TestXXX(t *testing.T) {
-	for _, test := range testCases {
-		actual, err := XXX(test.input)
+// func BenchmarkXXX(b *testing.B) {
+// 	b.StopTimer()
+// 	for _, test := range testCases {
+// 		b.StartTimer()
 
-		if actual != test.expected {
-			t.Fatalf("")
-		}
+// 		for i := 0; i < b.N; i++ {
+// 			XXX(test.input)
+// 		}
 
-		// if we expect an error and there isn't one
-		if test.expectErr && err == nil {
-			t.Errorf("Name(%v): expected an error, but error is nil", test.input)
-		}
-		// if we don't expect an error and there is one
-		if !test.expectErr && err != nil {
-			t.Errorf("Name(%v): expected no error, but error is %s", test.input, err)
-		}
-	}
-}
-
-func BenchmarkXXX(b *testing.B) {
-	b.StopTimer()
-	for _, test := range testCases {
-		b.StartTimer()
-
-		for i := 0; i < b.N; i++ {
-			XXX(test.input)
-		}
-
-		b.StopTimer()
-	}
-}
+// 		b.StopTimer()
+// 	}
+// }
